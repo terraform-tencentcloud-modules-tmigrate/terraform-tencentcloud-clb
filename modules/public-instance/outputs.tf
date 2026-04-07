a8cb3eaed388cb88b77d59cd448e899708371301
MaDongdong <donaldma@tencent.com>
2026-04-07 16:39:44 +0800

    add more modules

--- modules/public-instance/outputs.tf
+++ modules/public-instance/outputs.tf
@@ -6,3 +6,13 @@ output "clb_id" {
 output "clb_name" {
   value = local.name
 }
+
+output "clb_vips" {
+  description = "The virtual service address table of the CLB."
+  value       = var.create ? module.clb[0].clb_vips : []
+}
+
+output "clb_domain" {
+  description = "Domain name of the CLB instance."
+  value       = var.create ? module.clb[0].clb_domain : ""
+}
